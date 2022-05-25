d32eaa0a6191ec51f448f67c7deadd6139bd80fb
Robert McAbee <robertthemcabee@gmail.com>
2022-05-25 11:38:15 -0600

    Add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="fixed bottom-0 h-8 sm:right-0 w-full sm:w-min">
+      <div className="bg-neutral-content h-full w-full sm:w-[20rem] sm:rounded-tl-2xl">
+        <p className="pt-1.5 text-center text-base-300">
+          Built and curated by{" "}
+          <a
+            className="link link-hover"
+            href="https://twitter.com/RobertTheMcAbee"
+          >
+            @RobertTheMcAbee
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
