424f2151a6d87cf2ac03c39b588a1b0d73d6dd86
Robert McAbee <robertthemcabee@gmail.com>
2022-06-03 13:29:07 -0600

    minor fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  <>
-    <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    return <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
