87a89cb0fe1217d7464f272bb3df93e5f75f5b94
Robert McAbee <robertthemcabee@gmail.com>
2022-06-03 11:50:38 -0600

    change primary color

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -10,8 +10,8 @@ function Tag({
   toggleFilter: (tag: string) => void;
 }) {
   let tagClass = "m-1 p-3 badge hover:opacity-50";
-  if (filter.includes(tag)) tagClass += " badge-primary";
-  if (emphasis) tagClass += " px-8 border-1 border-primary";
+  if (filter.includes(tag)) tagClass += " font-bold text-teal-300";
+  if (emphasis) tagClass += " px-8 border-2 border-teal-300";
 
   return (
     <button onClick={() => toggleFilter(tag)} className={tagClass}>
