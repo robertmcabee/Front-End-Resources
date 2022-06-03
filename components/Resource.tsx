5f6a1040d451967dfe4c8270913fdf33178d9c73
Robert McAbee <robertthemcabee@gmail.com>
2022-06-03 11:52:08 -0600

    rework starred card appearance

--- components/Resource.tsx
+++ components/Resource.tsx
@@ -17,35 +17,27 @@ function Resource({
   filter: string[];
   toggleFilter: (tag: string) => void;
 }) {
+  let cardClasses =
+    "card z-0 shadow-xl group animate-fadein transition-all m-1";
+  if (data.starred) {
+    cardClasses += " bg-gradient-to-tl from-lime-300 via-teal-300 to-blue-400";
+  } else {
+    cardClasses += " unstarred";
+  }
+
   return (
-    <div className="card bg-base-300 shadow-xl animate-fadein">
+    <div className={cardClasses}>
       <div
-        className="absolute top-4 right-4"
-        style={data.starred ? { display: "relative" } : { display: "none" }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-green-300"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={0.5}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-          />
-        </svg>
-      </div>
-      <div className="card-body">
-        <a target="_blank" href={data.url} rel="noreferrer" className="group">
-          <h2 className="dark:text-white group-hover:opacity-50 pb-4 transition-all link card-title">
+        style={data.starred ? { position: "absolute" } : { display: "none" }}
+        className=" z-20 bg-gradient-to-b from-base-300 to-transparent opacity-80 h-full w-full group-hover:translate-y-[-100%] translate-x-[-0.5rem] transition-all duration-[500ms]"
+      ></div>
+
+      <div className="card-body z-20">
+        <a target="_blank" href={data.url} rel="noreferrer">
+          <h2 className="hover:opacity-50 pb-4 transition-all link card-title">
             {data.name}
           </h2>
-          <p className="group-hover:opacity-50 transition-colors">
-            {data.description}
-          </p>
+          <p className="transition-colors">{data.description}</p>
         </a>
         <div className="h-full"></div>
         <div className="flex flex-wrap">
@@ -60,10 +52,6 @@ function Resource({
               />
             );
           })}
-          <span
-            style={data.starred ? { display: "block" } : { display: "none" }}
-            className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-primary via-blue-400 to-green-300"
-          ></span>
         </div>
       </div>
     </div>
